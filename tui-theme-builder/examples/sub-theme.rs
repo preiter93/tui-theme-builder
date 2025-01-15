40cb762088f99ef958663c7afba533d0619512f7
Philipp Reiter <79789337+preiter93@users.noreply.github.com>
2025-01-15 22:52:31 +0100

    Update sub-theme.rs

--- tui-theme-builder/examples/sub-theme.rs
+++ tui-theme-builder/examples/sub-theme.rs
@@ -7,7 +7,16 @@ pub struct Colors {
     pub primary: Color,
 }
 
-#[derive(Debug, ThemeBuilder, PartialEq, Eq)]
+impl Default for Colors {
+    fn default() -> Self {
+        let s = r##"
+        "primary" = "#000000"
+        "##;
+        toml::from_str(s).unwrap()
+    }
+}
+
+#[derive(Debug, ThemeBuilder)]
 #[builder(context=Colors)]
 pub struct Theme {
     /// Annotate styles with 'fg=color', 'bg=color' or add modifiers,
@@ -20,40 +29,15 @@ pub struct Theme {
     pub sub_theme: Subtheme,
 }
 
-#[derive(Debug, ThemeBuilder, PartialEq, Eq)]
+#[derive(Debug, ThemeBuilder)]
 #[builder(context=Colors)]
 pub struct Subtheme {
     #[style(fg=primary, bg=primary)]
     pub base: Style,
 }
 
-impl Default for Colors {
-    fn default() -> Self {
-        let s = r##"
-        "primary" = "#000000"
-        "##;
-        toml::from_str(s).unwrap()
-    }
-}
-
 fn main() {
     let colors = Colors::default();
     let theme = Theme::build(&colors);
     println!("{theme:#?}");
-
-    assert_eq!(
-        theme,
-        Theme {
-            base: Style::default()
-                .fg(ratatui::style::Color::Rgb(0, 0, 0))
-                .bg(ratatui::style::Color::Rgb(0, 0, 0))
-                .bold()
-                .underlined(),
-            sub_theme: Subtheme {
-                base: Style::default()
-                    .fg(ratatui::style::Color::Rgb(0, 0, 0))
-                    .bg(ratatui::style::Color::Rgb(0, 0, 0)),
-            }
-        }
-    );
 }
